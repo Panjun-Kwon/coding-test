af9698220358a276ad4051a6df0506fb1a2a7c54
Panjun-Kwon <pndlm1979@naver.com>
2023-12-13 00:01:23 +0900

    2023/12/12 - 3문제

---
+++ src/programmers/level_1/No_133502.java
@@ -0,0 +1,39 @@
+package programmers.level_1;
+
+public class No_133502 {
+    public int solution(int[] ingredient) { // 실행 시간 초과
+        int answer = 0;
+        String bugger = "1231";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : ingredient) {
+            sb.append(i);
+        }
+        String str = sb.toString();
+
+        while(str.contains(bugger)) {
+            str = str.replaceFirst(bugger, "");
+            answer++;
+        }
+
+        return answer;
+    }
+
+    // https://programmer-may.tistory.com/154
+    public int solution2(int[] ingredient) {
+        int answer = 0;
+        String bugger = "1231";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : ingredient) {
+            sb.append(i);
+
+            if(sb.length() > 3 && sb.substring(sb.length() - 4, sb.length()).equals(bugger)) {
+                sb.delete(sb.length() - 4, sb.length());
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
